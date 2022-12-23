6cf722164162ba8cd84ebc27ab2a0b19b5b85af3
BHznJNs <441768875@qq.com>
2022-12-23 23:42:03 +0800

    Used `std::env::current_dir`

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,44 @@
 mod get_weather;
 mod read_config;
 mod write_data;
+
 use crate::get_weather::index::get_weather;
 use crate::read_config::index::read_config;
 use crate::write_data::index::write_data;
 
-use json::JsonValue;
-use json::JsonError;
+use std::env::current_dir;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut api_key: Option<String> = None;
     let mut city: Option<String> = None;
-    read_config(&mut api_key, &mut city);
+    read_config(&mut api_key, &mut city)?;
 
     if api_key == None {
         panic!("Wrong key to API.");
     }
 
-    let weather_res =
-        get_weather(api_key, city).await;
-    let weather_data: Result<JsonValue, JsonError>;
-    match weather_res {
-        Ok(s) => {
-            // let weather_data_str = Some(s.clone());
-            weather_data = json::parse(&s);
-        },
-        Err(_e) => {
-            panic!("Get weather error.");
-        }
-    }
-    match weather_data {
-        Ok(data) => {
-            let status = data["status"].clone();
-            if status == "1" {
-                let live_val = data["lives"][0].clone();
-                let weather = live_val["weather"].clone();
-                let temperature = live_val["temperature"].clone();
+    let weather_str =
+        get_weather(api_key, city).await?;
+    let weather_data = json::parse(&weather_str)?;
+    
+    // start analyse data
+    let status = weather_data["status"].clone();
+    if status == "1" {
+        let live_val = weather_data["lives"][0].clone();
+        let weather = live_val["weather"].clone();
+        let temperature = live_val["temperature"].clone();
 
-                println!("Weather: {}", weather);
-                println!("Temperature: {}", temperature);
+        println!("Weather: {}", weather);
+        println!("Temperature: {}", temperature);
 
-                #[warn(unused_must_use)]
-                write_data(String::from("./weather.txt"), weather.to_string());
-                write_data(String::from("./temperature.txt"), temperature.to_string());
-            }
-        },
-        Err(_e) => {},
+        let base = current_dir()?;
+        let weather_path = format!("{}/weather.txt", base.display());
+        let temperature_path = format!("{}/temperature.txt", base.display());
+
+        write_data(String::from(weather_path), weather.to_string())?;
+        write_data(String::from(temperature_path), temperature.to_string())?;
     }
+    Ok(())
 }
--- src/read_config/index.rs
+++ src/read_config/index.rs
@@ -1,28 +1,19 @@
 use crate::read_config::check_conf::check_conf;
 
 use configparser::ini::Ini;
+use std::error::Error;
+use std::env::current_dir;
 
-pub fn read_config( key: &mut Option<String>, city: &mut Option<String>) {
-    const CONF_PATH: &str = "./config.ini";
+pub fn read_config( key: &mut Option<String>, city: &mut Option<String>) -> Result<(), Box<dyn Error>> {
+    let path = current_dir()?;
+    let conf_path = format!("{}/config.ini", path.display());
 
-    let is_conf_created = check_conf(CONF_PATH);
-    match is_conf_created {
-        Ok(_s) => {},
-        Err(_e) => {
-            panic!("Config file create error!");
-        }
-    }
+    check_conf(conf_path.as_str())?;
 
     let mut config = Ini::new();
-    let __map = config.load(CONF_PATH);
-
-    match __map {
-        Ok(_map) => {
-            *key = config.get("default", "key");
-            *city = config.get("default", "city");
-        },
-        Err(_map) => {
-            panic!("Open config file error.");
-        }
-    }
+    let __map = config.load(conf_path)?;
+    
+    *key = config.get("default", "key");
+    *city = config.get("default", "city");
+    Ok(())
 }
